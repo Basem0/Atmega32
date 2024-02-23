db0b1ebdb9f725142ade26cc27e046ce1a65b24b
Ahmed Basem <126205898+Basem0@users.noreply.github.com>
2024-02-24 01:52:21 +0200

    Add files via upload

---
+++ Drivers/MCAL_LAYER/Timer0/Timer0_Config.h
@@ -0,0 +1,22 @@
+/*
+ * Timer0_Config.h
+ *
+ * Created: 2/23/2024 7:42:41 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef TIMER0_CONFIG_H_
+#define TIMER0_CONFIG_H_
+
+
+#include "Timer0.h"
+
+#define TIMER_CLOCK_SELECT					TIMER_Prescaler1024
+#define COMPARE_MATCH_OUTPUT_MODE			OC0_DISCONNECTED
+#define WAVEFORM_GENERATION_MODE			TIMER0_NORMAL_MODE
+
+
+
+
+#endif /* TIMER0_CONFIG_H_ */
